6571af6ad3d343cda2736dcfecc0585f5ebfc072
poemaman <luis.campos.pousa@gmail.com>
2024-02-20 11:14:27 +0100

    fecth2

---
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/books/pages/books-page/books-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AwardsPageComponent } from './awards-page.component';
+
+describe('AwardsPageComponent', () => {
+  let component: AwardsPageComponent;
+  let fixture: ComponentFixture<AwardsPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AwardsPageComponent]
+    });
+    fixture = TestBed.createComponent(AwardsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
