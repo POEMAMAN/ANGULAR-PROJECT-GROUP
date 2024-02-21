c96c5edd193cfadb1526df442c4918954276102e
poemaman <luis.campos.pousa@gmail.com>
2024-02-21 20:33:50 +0100

    feat: Finish GauntW40K Module

---
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/gaunt-w40k-universe/gauntW40KCharacters/pages/gauntW40KCharacters-page/gauntW40KCharacters-page.component.ts
@@ -0,0 +1,24 @@
+import { GauntW40KCharacter } from '../../interfaces/GauntW40KCharacters.interface';
+import { gauntW40KCharactersService } from '../../services/gauntW40KCharacters.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gauntW40KCharacters-page',
+  templateUrl: './gauntW40KCharacters-page.component.html',
+  styleUrls: ['./gauntW40KCharacters-page.component.css']
+})
+export class gauntW40KCharactersPageComponent implements OnInit {
+  gauntW40KCharacters: GauntW40KCharacter[] = []
+  constructor(private gauntW40KCharactersService: gauntW40KCharactersService ) {}
+
+
+  ngOnInit(){
+    this.gauntW40KCharactersService.getGauntW40KCharacters().subscribe({
+      next: (gauntW40KCharacters: GauntW40KCharacter[]) => {
+        this.gauntW40KCharacters = gauntW40KCharacters
+      },
+      error: () => {}
+    })
+  }
+
+}
--- ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/old-man-war-universe/old-man-war-universe-routing.module.ts
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/old-man-war-universe/old-man-war-universe-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { verifyTokenGuard } from 'src/app/modules/auth/guards/verify-token.guard';
+import { OldManWarUniverseComponent } from './old-man-war-universe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', children: [
+  {
+    path: '',canActivate: [verifyTokenGuard], component: OldManWarUniverseComponent
+  },
+  {
+    path: 'books', canActivate: [verifyTokenGuard],loadChildren: () => import('../old-man-war-universe/oldManWarBooks/oldManWarBooks.module').then(m => m.oldManWarBooksModule),
+  },
+  {
+    path: 'characters', canActivate: [verifyTokenGuard],loadChildren: () => import('../old-man-war-universe/oldManWarCharacters/oldManWarCharacters.module').then(m => m.oldManWarCharactersModule),
+  },
+  {
+    path: 'countries', canActivate: [verifyTokenGuard],loadChildren: () => import('../old-man-war-universe/oldManWarCountries/oldManWarCountries.module').then(m => m.oldManWarCountriesModule),
+  },
+  {
+    path: 'planets', canActivate: [verifyTokenGuard],loadChildren: () => import('../old-man-war-universe/oldManWarPlanets/oldManWarPlanets.module').then(m => m.oldManWarPlanetsModule),
+  },
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
