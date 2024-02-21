1fb4ce18db72aa8fe5a7d396f734e829c165fe70
poemaman <luis.campos.pousa@gmail.com>
2024-02-21 14:52:29 +0100

    feat: Finish OldManWar Module

--- ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/UltraW40K-Universe/ultra-w40k-universe-routing.module.ts
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/UltraW40K-Universe/ultra-w40k-universe-routing.module.ts
@@ -1,30 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UltraW40KBooksComponent } from './UltraW40KBooks/components/UltraW40KBooks/UltraW40KBooks.component';
-import { verifyTokenGuard } from 'src/app/modules/auth/guards/verify-token.guard';
-import { UltraW40KUniverseComponent } from './ultra-w40k-universe.component';
 
-const routes: Routes = [
-  {
-    path: '', children: [
-  {
-    path: '',canActivate: [verifyTokenGuard], component: UltraW40KUniverseComponent
-  },
-  {
-    path: 'books', canActivate: [verifyTokenGuard],loadChildren: () => import('../UltraW40K-Universe/UltraW40KBooks/UltraW40KBooks.module').then(m => m.UltraW40KBooksModule),
-  },
-  {
-    path: 'characters', canActivate: [verifyTokenGuard],loadChildren: () => import('../UltraW40K-Universe/UltraW40KCharacters/UltraW40KCharacters.module').then(m => m.UltraW40KCharactersModule),
-  },
-  {
-    path: 'countries', canActivate: [verifyTokenGuard],loadChildren: () => import('../UltraW40K-Universe/UltraW40KCountries/UltraW40KCountries.module').then(m => m.UltraW40KCountriesModule),
-  },
-  {
-    path: 'planets', canActivate: [verifyTokenGuard],loadChildren: () => import('../UltraW40K-Universe/UltraW40KPlanets/UltraW40KPlanets.module').then(m => m.UltraW40KPlanetsModule),
-  },
-  ]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/seaford-universe/seaford-universe-routing.module.ts
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/seaford-universe/seaford-universe-routing.module.ts
@@ -1,29 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { verifyTokenGuard } from 'src/app/modules/auth/guards/verify-token.guard';
-import { SeafordUniverseComponent } from './seaford-universe.component';
 
-const routes: Routes = [
-  {
-    path: '', children: [
-  {
-    path: '',canActivate: [verifyTokenGuard], component: SeafordUniverseComponent
-  },
-  {
-    path: 'books', canActivate: [verifyTokenGuard],loadChildren: () => import('../seaford-universe/seafordBooks/seafordBooks.module').then(m => m.seafordBooksModule),
-  },
-  {
-    path: 'characters', canActivate: [verifyTokenGuard],loadChildren: () => import('../seaford-universe/seafordCharacters/seafordCharacters.module').then(m => m.seafordCharactersModule),
-  },
-  {
-    path: 'countries', canActivate: [verifyTokenGuard],loadChildren: () => import('../seaford-universe/seafordCountries/seafordCountries.module').then(m => m.seafordCountriesModule),
-  },
-  {
-    path: 'planets', canActivate: [verifyTokenGuard],loadChildren: () => import('../seaford-universe/seafordPlanets/seafordPlanets.module').then(m => m.seafordPlanetsModule),
-  },
-  ]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
