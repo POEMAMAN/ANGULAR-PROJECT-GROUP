c7870bf4e0e9ad5361f404d83e16252c50cd5a66
poemaman <luis.campos.pousa@gmail.com>
2024-02-24 16:56:07 +0100

    chore: adaptation to scss oldmanwar

--- ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/old-man-war-universe/oldManWarPlanets/pages/oldManWarPlanets-page/oldManWarPlanets-page.component.ts
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/old-man-war-universe/oldManWarPlanets/pages/oldManWarPlanets-page/oldManWarPlanets-page.component.ts
@@ -6,7 +6,7 @@ import { oldManWarPlanetsService } from '../../services/oldManWarPlanets.service
 @Component({
   selector: 'app-oldManWarPlanets-page',
   templateUrl: './oldManWarPlanets-page.component.html',
-  styleUrls: ['./oldManWarPlanets-page.component.css']
+  styleUrls: ['./oldManWarPlanets-page.component.scss']
 })
 export class oldManWarPlanetsPageComponent implements OnInit {
   oldManWarPlanets: OldManWarPlanet[] = []
