27f9b4f78e1307573ed43751250b4c087b5a75d6
poemaman <luis.campos.pousa@gmail.com>
2024-02-24 08:18:35 +0100

    chore: change css for scss

--- ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/uplift-universe/uplift-universe.component.ts
+++ ANGULAR-LIBRARY/FRONTEND/src/app/modules/library/complete-universe/uplift-universe/uplift-universe.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uplift-universe',
   templateUrl: './uplift-universe.component.html',
-  styleUrls: ['./uplift-universe.component.css']
+  styleUrls: ['./uplift-universe.component.scss']
 })
 export class UpliftUniverseComponent {
 
